3764ec92b72904d9994906f651292e8158182f71
Marcelino <54931609+marcelino-abou-haidar@users.noreply.github.com>
2024-03-26 15:20:25 +0200

    refactor: clean code

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ const http = require('http');
 const { v4: uuidv4 } = require('uuid');
 const { EVENT_TYPES } = require('./utils/constants');
 
-const PORT = 3000;
 const server = http.createServer();
 const ws = new WebSocketServer({ server });
-const connections = {};
+const PORT = 3000;
 const users = {};
 const rooms = {};
+const connections = {};
 
 server.listen(PORT, () => {
   console.log(`Backend server started..`);
@@ -17,73 +17,35 @@ server.listen(PORT, () => {
 
 const broadCastConnectedUsers = (roomId) => {
   const room = rooms[roomId];
-
   if (!room) {
     console.log(`Room with ID ${roomId} not found.`);
     return;
   }
-
   const usersInRoom = room.map((client) => ({
     userId: client.userId,
     username: users[client.userId].username,
     isPainting: users[client.userId].state.isPainting,
   }));
-
   const message = JSON.stringify({
     type: EVENT_TYPES.USERS_LIST,
     data: usersInRoom,
   });
-
   room.forEach((client) => {
     client.client.send(message);
   });
 };
 
-const broadcastUserDisconnection = (userId) => {
-  const username = users[userId].username;
-  const message = JSON.stringify({
-    type: EVENT_TYPES.ALERT_EVENT,
-    message: {
-      alert: `User ${username} has disconnected.`,
-    },
-  });
-
-  broadcastMessage(userId, message);
-};
-
-const broadCastCanvasDrawing = (userId, message) => {
-  const { roomId } = users[userId];
-
-  if (!roomId || !rooms[roomId]) {
-    console.log(`User ${userId} is not in a valid room.`);
-    return;
-  }
-
-  const jsonMessage = JSON.stringify(message);
-
-  rooms[roomId].forEach((client) => {
-    if (client.userId === userId) {
-      return;
-    }
-
-    client.client.send(jsonMessage);
-  });
-};
-
-const broadCastCanvasClear = (userId, message) => {
+const broadCastCanvasState = (userId, message) => {
   const { roomId } = users[userId];
   if (!roomId || !rooms[roomId]) {
     console.log(`User ${userId} is not in a valid room.`);
     return;
   }
-
   const jsonMessage = JSON.stringify(message);
-
   rooms[roomId].forEach((client) => {
     if (client.userId === userId) {
       return;
     }
-
     client.client.send(jsonMessage);
   });
 };
@@ -92,36 +54,27 @@ const broadCastUserPaintingStatus = (userId, message) => {
   if (!users[userId]) {
     return;
   }
-
   const { roomId } = users[userId];
-
   if (!roomId || !rooms[roomId]) {
     console.log(`User ${userId} is not in a valid room.`);
     return;
   }
-
   const isPainting = message.data.isPainting;
-
   users[userId].state.isPainting = isPainting;
-
   broadCastConnectedUsers(roomId);
 };
 
 const onClose = (userId) => {
   const user = users[userId];
-
   if (!user) {
     console.log(`User not found..`);
     return;
   }
-
   const { roomId } = user;
-
   if (!roomId || !rooms[roomId]) {
     delete users[userId];
     return;
   }
-
   const room = rooms[roomId];
   const index = room.findIndex((client) => client.userId === userId);
   if (index !== -1) {
@@ -133,11 +86,7 @@ const onClose = (userId) => {
   }
 };
 
-const handleCreateRoom = (userId, { message }) => {
-  const roomId = uuidv4();
-  const client = connections[userId];
-  const username = message.username;
-
+const addUserToUsersList = (userId, username, roomId) => {
   users[userId] = {
     username,
     roomId,
@@ -145,6 +94,17 @@ const handleCreateRoom = (userId, { message }) => {
       isPainting: false,
     },
   };
+};
+
+const addUserToRoom = (roomId, userId, client, existing) => {
+  if (existing) {
+    rooms[roomId].push({
+      userId,
+      client,
+    });
+
+    return;
+  }
 
   rooms[roomId] = [
     {
@@ -152,16 +112,21 @@ const handleCreateRoom = (userId, { message }) => {
       client,
     },
   ];
+};
 
+const handleCreateRoom = (userId, { message }) => {
+  const roomId = uuidv4();
+  const client = connections[userId];
+  const username = message.username;
+  addUserToUsersList(userId, username, roomId);
+  addUserToRoom(roomId, userId, client);
   const messageToSend = JSON.stringify({
     type: EVENT_TYPES.ROOM_CREATED,
     message: {
       roomId,
     },
   });
-
   client.send(messageToSend);
-
   setTimeout(() => {
     broadCastConnectedUsers(roomId);
   }, 500);
@@ -171,15 +136,7 @@ const handleRoomJoin = (userId, { message }) => {
   const username = message.username;
   const roomId = message.roomId;
   const client = connections[userId];
-
-  users[userId] = {
-    username,
-    roomId,
-    state: {
-      isPainting: false,
-    },
-  };
-
+  addUserToUsersList(userId, username, roomId);
   if (rooms[roomId] === undefined) {
     const message = JSON.stringify({
       type: EVENT_TYPES.ALERT_EVENT,
@@ -187,39 +144,29 @@ const handleRoomJoin = (userId, { message }) => {
         data: `This room does not exist.`,
       },
     });
-
     client.send(message);
-
     console.log(
       `User ${username} tried to connect to a room that does not exist.`
     );
-
     return;
   }
 
-  rooms[roomId].push({
-    userId,
-    client,
-  });
-
+  addUserToRoom(roomId, userId, client, true);
   const messageToSend = JSON.stringify({
     type: EVENT_TYPES.ROOM_JOINED,
     message: {
       roomId,
     },
   });
-
   client.send(messageToSend);
-
   broadCastConnectedUsers(roomId);
-
   console.log(`User ${username} joined room with the ID of ${roomId}`);
 };
 
 const onMessage = (userId, message) => {
   const jsonMessage = JSON.parse(message.toString());
-
-  switch (jsonMessage.type) {
+  const { type } = jsonMessage;
+  switch (type) {
     case EVENT_TYPES.ROOM_CREATE:
       handleCreateRoom(userId, jsonMessage);
       break;
@@ -227,10 +174,8 @@ const onMessage = (userId, message) => {
       handleRoomJoin(userId, jsonMessage);
       break;
     case EVENT_TYPES.CANVAS_DRAW:
-      broadCastCanvasDrawing(userId, jsonMessage);
-      break;
     case EVENT_TYPES.CANVAS_CLEAR:
-      broadCastCanvasClear(userId, jsonMessage);
+      broadCastCanvasState(userId, jsonMessage);
       break;
     case EVENT_TYPES.USER_PAINTING:
       broadCastUserPaintingStatus(userId, jsonMessage);
